62ab02c3dd0e136c3f716f6ebdb6e11518628656
John-Wambua <jwambua307@gmail.com>
2020-05-22 17:47:59 +0300

    Dynamic note rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 const App=()=>{
     return <div>
         <Header/>
-        <Note/>
+        {notes.map(currentNote=>
+            <Note
+                key={currentNote.key}
+                title={currentNote.title}
+                content={currentNote.content}
+            />
+        )}
         <Footer/>
     </div>
 }
